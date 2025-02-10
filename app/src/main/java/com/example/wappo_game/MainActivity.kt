5791c9a84516b860aeebc63c2ea045e040585efa
CNJerry-IvanovVyacheslav <syava11212@yandex.ru>
2025-02-10 13:18:57 +0300

    made a base of switch theme in settings

--- app/src/main/java/com/example/wappo_game/MainActivity.kt
+++ app/src/main/java/com/example/wappo_game/MainActivity.kt
@@ -1,21 +1,30 @@
 package com.example.wappo_game
 
+import android.content.res.Resources.Theme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.res.ResourcesCompat.ThemeCompat
 import androidx.navigation.NavController
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
@@ -119,8 +128,30 @@ fun Make_custom_level() {
 @Composable
 fun Settings() {
     Text("Settings", fontSize = 30.sp)
-
-
+    Box {
+        val checkedState = remember { mutableStateOf(false) }
+        val textColor = remember { mutableStateOf(Color.Unspecified) }
+        Column {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(
+                    "Темная тема",
+                    fontSize = 22.sp,
+                    color = textColor.value,
+                    modifier = Modifier.padding(8.dp, 100.dp)
+                )
+                Switch(
+                    checked = checkedState.value,
+                    onCheckedChange = {
+                        checkedState.value = it
+                        if (checkedState.value) {
+                            TODO("Make switch theme")
+                            textColor.value = Color(0xFF6650a4)
+                        } else textColor.value = Color.Unspecified
+                    }
+                )
+            }
+        }
+    }
 }
 
 sealed class NavRoutes(val route: String) {
